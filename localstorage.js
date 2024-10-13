9dd72001b83e1d67f61ca14f14cdcca2f63fda30
Shahab Uddin <shahab.82946@gmail.com>
2024-10-13 18:03:09 +0600

    aking function to save data to local storage and display data on ui

--- localstorage.js
+++ localstorage.js
@@ -3,11 +3,53 @@ console.log('hello from js');
 const addProduct = () => {
   const productName = document.getElementById('product_name');
   const productQuantity = document.getElementById('product_quantity');
-  const productField = productName.value;
-  const productQuantityField = productQuantity.value;
+  const product = productName.value;
+  const quantity = productQuantity.value;
 
   productName.value = '';
   productQuantity.value = '';
-  console.log(productField,productQuantityField );
-  
+  console.log(product,quantity );
+  displayProduct(product, quantity)
+  saveToLocalStorage(product, quantity)
 }
+
+const displayProduct = (product, quantity) =>{
+  const productContainer = document.getElementById('product_container');
+  const li = document.createElement('li');
+  li.innerText =`${product}: ${quantity}`;
+  productContainer.appendChild(li)
+}
+
+// get stored item from local storage ---if cart found with value as cart it  will return. if cart found with empty it will return empty cart;
+const getStoredShoppingCart = () => {
+  const storedCart = localStorage.getItem('cart');
+
+  let cart = {}
+  if(storedCart) {
+    cart = JSON.parse(storedCart)
+  }
+  return cart;
+
+}
+
+
+const saveToLocalStorage = (product, quantity) => {
+
+  const cart = getStoredShoppingCart();
+  cart[product] = quantity;
+  const cartStringified = JSON.stringify(cart)
+  localStorage.setItem('cart', cartStringified)
+
+}
+
+
+const displayProductsFromLocalstorage = () => {
+  const savedCart = getStoredShoppingCart();
+  for(const product in savedCart){
+    const quantity = savedCart[product]
+   displayProduct(product, quantity)
+   console.log(savedCart);
+  }
+}
+
+displayProductsFromLocalstorage()
